90b345831e2cb439305d5f18c7907f2885b7186b
Walter Bormans <walter.bormans@paradoxcat.com>
2020-12-19 14:15:36 +0100

    Sets up skeletal structure of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import fetchComicInfo, { xkcdInfo } from './XKCDApiApi'
+import Overview from './components/overview';
+import DetailView from './components/detail_view';
 
 function App() {
+  const [openComic, setOpenComic] = useState<number>(0);
+  const [comicInfo, setComicInfo] = useState<xkcdInfo|null>(null);
+  useEffect(() => {
+    fetchComicInfo().then(result => setComicInfo(result))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={() => setOpenComic((comic) => comic + 1)}>
+      <h1>{comicInfo ? comicInfo.num : ''} – {comicInfo ? comicInfo.title : "Loading comic..."}</h1>
+      {openComic > 0 ? <DetailView number={openComic}></DetailView> : <Overview></Overview>}
     </div>
   );
 }
