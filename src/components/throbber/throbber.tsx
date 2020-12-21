ece224176996faf81b4be71447cd587c1fe5b626
Walter Bormans <walter.bormans@paradoxcat.com>
2020-12-21 22:33:15 +0100

    Adds throbber component

---
+++ src/components/throbber/throbber.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import styles from './throbber.module.css'
+
+type ThrobberProps = {
+    color?: string
+    className?: string
+}
+
+const Throbber: React.FC<ThrobberProps> = ({ color, className = '' }) => <div className={styles.container + ' ' + className }>
+    <div style={{background: color}} />
+    <div style={{background: color}} />
+    <div style={{background: color}} />
+</div>
+export default Throbber
+
+// Throbber based on examples from here: https://loading.io/css/
