cec11c30bd7492b8caa5977c37fbfb72d70b8827
Walter Bormans <walter.bormans@paradoxcat.com>
2020-12-21 13:20:33 +0100

    Moves thumbnail style to style document

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import fetchComicInfo, { xkcdInfo } from '../XKCDApiApi'
-
+import styles from './thumbnail.module.css'
 
 type ThumbnailType = {
     comicId: number
     onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+    className?: string
 }
 
-const Thumbnail: React.FC<ThumbnailType> = ({comicId, onClick}) => {
-    const [comicInfo, setComicInfo] = useState<xkcdInfo|null>(null);
+const Thumbnail: React.FC<ThumbnailType> = ({ comicId, onClick, className = '' }) => {
+    const [comicInfo, setComicInfo] = useState<xkcdInfo | null>(null);
     useEffect(() => {
         // Get the info for this comic.
         fetchComicInfo(comicId).then(result => setComicInfo(result))
     }, [comicId])
-
-    return <div style={{ width: 100, height: 100, border: "solid red" }} onClick={onClick}>
-        <img style={{ width: 100, height: 100 }} src={comicInfo?.img} />
+    
+    return <div onClick={onClick} className={ className + ' ' + styles.container}>
+        <img src={comicInfo?.img} alt={comicInfo?.title} className={styles.main} />
     </div>;
 };
 
