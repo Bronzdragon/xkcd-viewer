96cace165ca9abb5504d752636d3c9cdda950f9b
Walter Bormans <walter.bormans@paradoxcat.com>
2020-12-23 22:40:05 +0100

    Re-writing app to move logic down the tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
 import React, { useEffect, useState } from 'react';
 
-import fetchComicInfo, { preloadImage } from './XKCDApiApi'
+import fetchComicInfo, { preloadImage, xkcdInfo } from './XKCDApiApi'
 import Overview from './components/overview';
 import DetailView from './components/detail_view';
 
 function App() {
-    const [mostRecentComic, setMostRecentComic] = useState(0);
-    const [openComic, setOpenComic] = useState(0);
+    const [firstComic, setFirstComic] = useState<xkcdInfo|null>(null)
+    const [latestComic, setLatestComic] = useState<xkcdInfo|null>(null)
+    const [openComic, setOpenComic] = useState(0)
     useEffect(() => {
-        fetchComicInfo().then(result => setMostRecentComic(result.number))
+        fetchComicInfo().then(result => setLatestComic(result))
+        fetchComicInfo(1).then(setFirstComic)
     }, [])
 
     // Preload the next/previous comic pages, if there are any.
     useEffect(() => {
-        if (openComic < mostRecentComic) { fetchComicInfo(openComic + 1).then(({ img }) => preloadImage(img)) }
+        if (openComic < (latestComic?.number ?? 0)) { fetchComicInfo(openComic + 1).then(({ img }) => preloadImage(img)) }
         if (openComic > 1) { fetchComicInfo(openComic - 1).then(({ img }) => preloadImage(img)) }
-    }, [openComic, mostRecentComic])
+    }, [openComic, latestComic])
 
-    if (mostRecentComic === 0) {
+    if (!latestComic || !firstComic) {
         // No comics loaded yet...
         return <div>...loading</div>
     }
 
     if (!openComic) {
-        return <Overview onOpenComic={setOpenComic} latestComic={mostRecentComic} />
+        
+        return <Overview />
     } else {
         return <DetailView
             goBackHome={() => setOpenComic(0)}
             number={openComic}
-            nextComic={() => setOpenComic(num => Math.min(mostRecentComic, num + 1))}
+            nextComic={() => setOpenComic(num => Math.min(latestComic.number, num + 1))}
             previousComic={() => setOpenComic(num => Math.max(1, num - 1))}
         />
     }
--- src/components/overview.tsx
+++ src/components/overview.tsx
@@ -2,51 +2,63 @@ import React, { useEffect, useState } from 'react'
 import Preview from './preview/preview'
 import styles from './overview.module.css'
 import Navigator from './navigator/navigator'
+import fetchComicInfo, { xkcdInfo } from '../XKCDApiApi'
 
 type OverviewProps = {
-    onOpenComic: (id: number) => void
-    latestComic: number
+    onOpenComic?: (id: number) => void
+    latestComic?: number
 }
 
-const distanceFromBottom = () => document.body.scrollHeight - (window.pageYOffset + document.documentElement.clientHeight)
+const Overview: React.FC<OverviewProps> = (props: OverviewProps) => {
+    const [firstComic, setFirstComic] = useState<xkcdInfo|null>(null)
+    const [latestComic, setLatestComic] = useState<xkcdInfo|null>(null)
+    const [dateRange, setDateRange] = useState<[Date, Date]>()
 
-const generateThumbnails = (num: number, index: number, onOpenComic: (id: number) => void, ascending = false) => {
-    const incrementer = ascending ? 1 : -1
-    const newPreviews: JSX.Element[] = []
-
-    for (let i = index; i > index + (num * incrementer); i += incrementer) {
-        newPreviews.push(<Preview key={i} onClick={() => onOpenComic(i)} comicId={i} />,)
-    }
-
-    return newPreviews
-}
-
-const Overview: React.FC<OverviewProps> = ({ onOpenComic, latestComic }) => {
-    const [previews, setPreviews] = useState<JSX.Element[]>([])
 
     useEffect(() => {
-        const scrollHandler = () => {
-            if(distanceFromBottom() < 400) {
-                setPreviews(previews => [...previews, ...generateThumbnails(5, latestComic - previews.length, onOpenComic)])
-            }
-        }
-        document.addEventListener("scroll", scrollHandler )
-        return () => document.removeEventListener("scroll", scrollHandler)
-    })
+        fetchComicInfo().then(setLatestComic)
+        fetchComicInfo(1).then(setFirstComic)
+    }, [])
+    // const [previews, setPreviews] = useState<JSX.Element[]>([])
 
-    useEffect(() => {
-        if(distanceFromBottom() < 400) {
-            setPreviews(previews => [...previews, ...generateThumbnails(3, latestComic - previews.length, onOpenComic)])
-        }
-    }, [previews, latestComic, onOpenComic])
+    // useEffect(() => {
+    //     const scrollHandler = () => {
+    //         if(distanceFromBottom() < 400) {
+    //             setPreviews(previews => [...previews, ...generateThumbnails(5, latestComic - previews.length, onOpenComic)])
+    //         }
+    //     }
+    //     document.addEventListener("scroll", scrollHandler )
+    //     return () => document.removeEventListener("scroll", scrollHandler)
+    // })
+
+    // useEffect(() => {
+    //     if(distanceFromBottom() < 400) {
+    //         setPreviews(previews => [...previews, ...generateThumbnails(3, latestComic - previews.length, onOpenComic)])
+    //     }
+    // }, [previews, latestComic, onOpenComic])
 
     return <div>
-        Most recent comic: {latestComic}!
+        First comic: #{firstComic?.number} [{firstComic?.title}]<br />
+        Most recent comic: #{latestComic?.number} [{latestComic?.title}]!
+
         <div className={styles.container}>
-            {previews}
+            {/* {previews} */}
         </div>
         <Navigator setMonth={month => console.log(month)} viewFavourites={() => console.log("Here are your favourites: Strawberry icecream")} />
     </div>
 }
 
 export default Overview
+
+// const distanceFromBottom = () => document.body.scrollHeight - (window.pageYOffset + document.documentElement.clientHeight)
+
+// const generateThumbnails = (num: number, index: number, onOpenComic: (id: number) => void, ascending = false) => {
+//     const incrementer = ascending ? 1 : -1
+//     const newPreviews: JSX.Element[] = []
+
+//     for (let i = index; i > index + (num * incrementer); i += incrementer) {
+//         newPreviews.push(<Preview key={i} onClick={() => onOpenComic(i)} comicId={i} />,)
+//     }
+
+//     return newPreviews
+// }
