96dbaeae87b278aac7f191b43e9cc8f0c4ce00d4
Walter Bormans <walter.bormans@paradoxcat.com>
2020-12-30 22:07:09 +0100

    Adds previous/next buttons next to the date selector

--- src/components/navigator/navigator.tsx
+++ src/components/navigator/navigator.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
-import MonthSelector from '../month-selector/month-selector';
-import styles from './navigator.module.css'
 import type { DateRange } from '../overview'
+
+import MonthSelector from '../month-selector/month-selector';
+
+import chevron from './chevron.svg'
+import styles from './navigator.module.css'
+
 
 type navigatorProps = {
     setMonthRange: (start: Date, end: Date) => void
@@ -14,7 +18,10 @@ const Navigator = ({ setMonthRange, viewFavourites, dateRange, validDateRange }:
     const [useRange, setUseRange] = useState(false);
 
     return <div className={styles.container}>
+        <img src={chevron} className={[styles.chevron, styles.left].join(' ')}
+            onClick={() => setMonthRange(...adjustDateRange(dateRange, -1))} />
         <MonthSelector onChangeMonth={setMonthRange} useRange={useRange} dateRange={dateRange} validDateRange={validDateRange} />
+        <img src={chevron} className={[styles.chevron, styles.right].join(' ')} onClick={() => setMonthRange(...adjustDateRange(dateRange, +1))} />
         <span onClick={() => { setUseRange(use => !use) }}>
             &lt;Range toggle<input type="checkbox" checked={useRange} onChange={event => setUseRange(event.target.checked)} />&gt;
         </span>
@@ -23,3 +30,22 @@ const Navigator = ({ setMonthRange, viewFavourites, dateRange, validDateRange }:
 }
 
 export default Navigator
+
+function adjustDateRange([start, end]: DateRange, amount: number = 1): DateRange {
+    const startResult = new Date(start)    
+    const endResult = new Date(end)
+
+    startResult.setMonth(startResult.getMonth() - 1)
+    startResult.setDate(1)
+
+    endResult.setMonth(endResult.getMonth() - 1)
+    endResult.setDate(1)
+
+    return [startResult, endResult]
+
+    let result: DateRange = [
+        new Date(start.getFullYear(), start.getMonth() + amount),
+        new Date(end.getFullYear(), end.getMonth() + amount)
+    ]
+    return result
+}
