a399b18b2d918dd9eb176a23a2c5e513fb9a4485
Walter Bormans <walter.bormans@paradoxcat.com>
2020-12-30 18:05:02 +0100

    After feedback, removed half-star state

--- src/components/detail_view/footer/favourite_button.tsx
+++ src/components/detail_view/footer/favourite_button.tsx
@@ -1,18 +1,9 @@
-import { useState } from 'react';
-import Star, { StarType } from '../../star/star';
+import Star from '../../star/star';
 
 export default function FavouriteButton({ isFavourite, onToggleFavourite }: FavouriteButtonType) {
-    const [isHovering, setIsHovering] = useState(false);
-    const starState = isHovering ? StarType.Half : isFavourite ? StarType.Full : StarType.Empty;
-
     return <button
-        onMouseEnter={() => setIsHovering(true)}
-        onMouseLeave={() => setIsHovering(false)}
-        onClick={() => {
-            onToggleFavourite && onToggleFavourite();
-            setIsHovering(false); // Dumb hack to get the button to change.
-        }}>
-        Set favourite <Star type={starState} />
+        onClick={onToggleFavourite}>
+        Set favourite <Star filled={isFavourite} />
     </button>;
 }
 type FavouriteButtonType = {
--- src/components/star/star.tsx
+++ src/components/star/star.tsx
@@ -1,28 +1,22 @@
 import full_star from './full_star.svg'
-import half_star from './half_star.svg'
 import empty_star from './empty_star.svg'
 import styles from './star.module.css'
 
-export enum StarType {
-    Full, Half, Empty
-}
-
 type StarProps = {
-    type: StarType
+    filled: boolean
     size?: number
 }
 
 const starEnumToPictureMap = new Map([
-    [StarType.Full, { src: full_star, description: "Full star" }],
-    [StarType.Half, { src: half_star, description: "Half star" }],
-    [StarType.Empty, { src: empty_star, description: "Empty star" }],
+    [true, { src: full_star, description: "Full star" }],
+    [false, { src: empty_star, description: "Empty star" }],
 ])
 
-export default function Star({ type, size = 20 }: StarProps) {
+export default function Star({ filled, size = 20 }: StarProps) {
     return <img
         className={styles.star}
-        src={starEnumToPictureMap.get(type)?.src}
-        alt={starEnumToPictureMap.get(type)?.description}
+        src={starEnumToPictureMap.get(filled)?.src}
+        alt={starEnumToPictureMap.get(filled)?.description}
         width={size} height={size}
     />
 }
