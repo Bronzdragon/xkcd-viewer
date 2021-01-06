8134287564e790b4139d8d043cb05d0b66239e04
Walter Bormans <walter.bormans@paradoxcat.com>
2021-01-06 16:38:02 +0100

    Allow adding/removing favourites from the overview menu.

--- src/components/preview/preview.tsx
+++ src/components/preview/preview.tsx
@@ -5,7 +5,7 @@ import Throbber from "../throbber/throbber"
 
 import styles from './preview.module.css'
 import Star from "../star/star"
-import { isFavourite } from "../../favourites"
+import { isFavourite, addFavourite, removeFavourite } from "../../favourites"
 
 type PreviewType = {
     info: Promise<xkcdInfo>
@@ -14,9 +14,13 @@ type PreviewType = {
 
 const Preview: React.FC<PreviewType> = ({ info: infoPromise, onOpenComic }) => {
     const [info, setInfo] = useState<xkcdInfo>()
+    const [favourite, setFavourite] = useState(false)
 
     useEffect(() => {
-        infoPromise.then(info => setInfo(info))
+        infoPromise.then(info => {
+            setInfo(info)
+            setFavourite(isFavourite(info.number))
+        })
     }, [infoPromise])
 
     if (!info) {
@@ -26,9 +30,9 @@ const Preview: React.FC<PreviewType> = ({ info: infoPromise, onOpenComic }) => {
         </div>
     }
 
-    return <div className={styles.container} onClick={() => onOpenComic && onOpenComic(info.number)}>
-        <span>{info.title} <Star filled={isFavourite(info.number)} /></span>
-        <Thumbnail className={styles.thumbnail} comicInfo={info} />
+    return <div className={styles.container} >
+        <span onClick={() => { (favourite ? removeFavourite : addFavourite)(info.number); setFavourite(!favourite) }}>{info.title} <Star filled={favourite} /></span>
+        <Thumbnail className={styles.thumbnail} comicInfo={info} onClick={() => onOpenComic && onOpenComic(info.number)} />
     </div>
 }
 
--- src/components/thumbnail/thumbnail.tsx
+++ src/components/thumbnail/thumbnail.tsx
@@ -7,10 +7,11 @@ import styles from './thumbnail.module.css'
 export type ThumbnailType = {
     comicInfo: xkcdInfo
     className?: string
+    onClick?: (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
 }
 
-const Thumbnail: React.FC<ThumbnailType> = ({ comicInfo: { img: imgUrl, title }, className }) => {
-    return <img src={imgUrl} alt={title} title={title} className={cs(styles.image, className)} />;
+const Thumbnail: React.FC<ThumbnailType> = ({ comicInfo: { img: imgUrl, title }, className, onClick }) => {
+    return <img onClick={onClick} src={imgUrl} alt={title} title={title} className={cs(styles.image, className)} />;
 };
 
 export default Thumbnail
