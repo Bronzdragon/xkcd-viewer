de6b7594b287a5d6ff1fa1874c6b6954f2696180
Walter Bormans <walter.bormans@paradoxcat.com>
2021-01-06 20:03:40 +0100

    Adds onclick event passthrough for the star element

--- src/components/star/star.tsx
+++ src/components/star/star.tsx
@@ -5,6 +5,7 @@ import styles from './star.module.css'
 type StarProps = {
     filled: boolean
     size?: number
+    onClick?: (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
 }
 
 const starEnumToPictureMap = new Map([
@@ -12,11 +13,12 @@ const starEnumToPictureMap = new Map([
     [false, { src: empty_star, description: "Empty star" }],
 ])
 
-export default function Star({ filled, size = 20 }: StarProps) {
+export default function Star({ filled, size = 20, onClick }: StarProps) {
     return <img
         className={styles.star}
         src={starEnumToPictureMap.get(filled)?.src}
         alt={starEnumToPictureMap.get(filled)?.description}
         width={size} height={size}
+        onClick={onClick}
     />
 }
